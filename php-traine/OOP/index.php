6224a6d83002feeaf32c9c8ec77f75b3dbea06ce
RaptorZed <lion-zolo@rambler.ru>
2016-05-15 11:38:42 +0300

    push ls3

--- php-traine/OOP/index.php
+++ php-traine/OOP/index.php
@@ -2,31 +2,60 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>PHP - OOP test</title>
 </head>
 <body>
+
+<!--------------------------------------------->
+<!--ls 3
+    -стойства и методи
+-->
 <?php
-    class Room{
-        public function __construct()
-        {
-            echo ('Construct method');
-        }
-        public function __invoke()
-        {
-            echo ('Invoke method');
-        }
-        public function __toString()
-        {
-            echo('String method');
-        }
-    }
+//class Room{
+//    public $color = 'Yellow';
+//
+//    public function get_color(){
+//        print($this->color);
+//    }
+//
+//    public function change_color($col){
+//        $this->color = $col;
+//        print($this->color);
+//    }
+//}
+//$obj = new Room();
+//$obj-> change_color('Black');
+//?>
+<!--------------------------------------------->
 
 
-$obj = new Room();
-echo('<br/>');
-$obj();
-echo('<br/>');
-echo $obj;
-?>
+<!--------------------------------------------->
+<!--ls 6
+    __construct
+    __invoke
+    __toString
+-->
+<?php
+//    class Room{
+//        public function __construct()
+//        {
+//            echo ('Construct method');
+//        }
+//        public function __invoke()
+//        {
+//            echo ('Invoke method');
+//        }
+//        public function __toString()
+//        {
+//            echo('String method');
+//        }
+//    }
+//$obj = new Room();
+//echo('<br/>');
+//$obj();
+//echo('<br/>');
+//echo $obj;
+//?>
+<!--------------------------------------------->
 </body>
 </html>
