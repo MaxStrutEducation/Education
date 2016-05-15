16be9585832a25dd35f0130a63ab194143c42741
RaptorZed <lion-zolo@rambler.ru>
2016-05-15 11:46:47 +0300

    ls 3 - add test code

--- php-traine/OOP/ReadMe.txt
+++ php-traine/OOP/ReadMe.txt
@@ -1,2 +1,5 @@
 YouTube OOP course
 link - https://www.youtube.com/watch?v=Bo8M6g5waos&list=PL5QUj3iB7EoJGDMxuzlV58uh0ybgAHlcN&index=1
+
+GitHub repository
+link - https://github.com/MaxStrutEducation/Education/tree/master/php-traine/OOP
--- php-traine/OOP/index.php
+++ php-traine/OOP/index.php
@@ -14,17 +14,21 @@
 //class Room{
 //    public $color = 'Yellow';
 //
-//    public function get_color(){
+//    public function change_color($col){
+//        $this->color = $col;
 //        print($this->color);
 //    }
 //
-//    public function change_color($col){
-//        $this->color = $col;
+//    public function get_color(){
 //        print($this->color);
 //    }
 //}
 //$obj = new Room();
 //$obj-> change_color('Black');
+//echo ('<br/>');
+//$obj-> change_color('Tomato');
+//echo ('<br/>');
+//$obj-> get_color();
 //?>
 <!--------------------------------------------->
 
